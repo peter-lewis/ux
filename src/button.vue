8f41cb2dc3fd64eff25810c36941707ea7bd4e19
Peter Lewis <peter.lewis@glgroup.com>
2022-11-01 17:54:43 -0400

    some button

---
+++ src/button.vue
@@ -0,0 +1,12 @@
+<template>
+    <button @click="onClick">Click me</button>
+</template>
+<script>
+    export default {
+        methods: {
+            onClick() {
+                this.$emit('click');
+            }
+        }
+    }
+</script>
